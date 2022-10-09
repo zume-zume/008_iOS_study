9bc95760eb4f3465bcb22756b5a7bfe22000e86a
“zume-zume” <88725806+zume-zume@users.noreply.github.com>
2022-10-09 15:15:59 +0900

    first commit

---
+++ 008_iOS_study/sandbox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  008_iOS_study
+//
+//  Created by zume on 2022/10/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
